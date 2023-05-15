81eeb149df93e973b5d16590d1bf9b43535fc7dc
Suraj Kumar <surajsinhaking77@gmail.com>
2023-05-15 23:59:26 +0530

    update all

---
+++ recursion.py
@@ -0,0 +1,61 @@
+
+# def fun(a):
+#     print("Suraj Kumar")
+#     return a 
+
+# a = fun(5)
+# b = fun(9)
+
+# print(a, b)
+
+
+
+
+# a = 9
+
+# def fun(): 
+#     a = 8
+#     print(" funtiopn inside ", a)  
+
+# fun()
+
+# print("function outside ", a)
+
+
+
+
+
+
+# def counting(a):
+#     if(a > 10):
+#         return
+    
+#     print(a)
+#     # a+=1
+#     counting(a+1)
+
+# counting(1)
+
+
+
+# def two_table(a, b = 1):
+#     if b > 10:
+#         return 
+    
+#     print(a * b)
+#     two_table(a , b+1)
+
+
+# two_table(5)
+
+# 1 + 2 + 3 + 4
+
+def multiples(n):
+    if(n*2>20):
+        return
+    
+    print(n*2)
+    # n+=1
+    multiples(n+1)
+
+multiples(1)
---
+++ text.py
@@ -0,0 +1,31 @@
+
+#  1 2 3 4 5 6 8 9 10 14 16 19 22 23 25 26 27 29 31 34 35 36 38 39 40 45 46 48 50 51 52 57 59 60 61 63 67 68 69 71 75 76 77 79 81 82 83 86 87 88 90 92 93 94 95 96 98 99 100
+#  93
+
+def binaryS(arr, lb, ub, k):
+        if(lb<ub):   # 3 == 3
+            mid = int((lb+ub)/2)
+            if(k == arr[mid]):
+                return mid
+            elif(arr[mid] > k):
+                return binaryS(arr, lb, mid-1, k)
+            else:
+                return binaryS(arr, mid+1, ub, k)
+        else:
+            return -1
+
+
+import array as ar
+arr = ar.array('i', [])
+
+
+print(arr)
+for i in range(0,59):
+    a = int(input())
+    if(a == -1):
+        break
+    arr.append(a)
+
+item = int(input("item to find : "))
+
+print( binaryS(arr, 0, len(arr), item))
