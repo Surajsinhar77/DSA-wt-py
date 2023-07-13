0c03790660bec999fd7637ab3b4527a68e178d8e
Suraj Kumar <surajsinhaking77@gmail.com>
2023-07-13 21:39:11 +0530

    tree node

--- treeNode.py
+++ treeNode.py
@@ -23,11 +23,11 @@ class treenodeFunction:
 
         while(pendingNode.qsize() > 0):
             front = pendingNode.get() #   
-            n = int(input("how many Child do u want to enter : "))
+            n = int(input(f"how many Child do u want to enter : {front.data} : "))
             if(n > 0):
                 for i in range(0,n):
-                    data = int(input("Enter the Root data : "))
-                    newNode = treenode(data) # 9 7 4 8  
+                    data = int(input(f'Enter the {front.data} childrens data : '))
+                    newNode = treenode(data)
                     front.children.append(newNode)
                     pendingNode.put(newNode)
         return root
