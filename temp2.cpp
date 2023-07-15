dd03ce91c28bd9e1cdc839cd9c2742adaf9c9667
Suraj Kumar <surajsinhaking77@gmail.com>
2023-07-15 21:41:10 +0530

    display tree

--- temp2.cpp
+++ temp2.cpp
@@ -1,12 +1,52 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 
-int main(){
-    string ans;
 
-    getline(cin, ans);
-    cout<<ans;
+
+int main(){
+    // string ans;
+
+    // getline(cin, ans);
+    // cout<<ans;
+
+    vector<int> arry;
+
+    arry.push_back(2);
+    arry.push_back(4);
+
+    cout<<arry.size()<<endl;
+
+    int n = 5;
+
+    // for (int i =0; i<n; i++){
+    //     int data;
+    //     cin>>data;
+    //     arry.push_back(data);
+    // }
+
+    for (int i =0; i<n; i++){
+        int data;
+        cout<<" enter data : ";
+        cin>>data;
+        arry.push_back(data);
+    }
+
+    // int ary[6];
+
+    // for(int i =0; i<6; i++){
+    //     cin>>ary[i];
+    // }
+
+    arry.pop_back();
+
+    for( int i =0; i<arry.size(); i++){
+        // cout<<arry[i]<<" ";
+        cout<<arry.at(i)<<" ";
+    }
+    
+    
     return 0;
 }
--- treeNode.py
+++ treeNode.py
@@ -3,8 +3,10 @@ from queue import Queue
 
 class treenode:
     data = 0
-    children = []
+    childern = []
+
     def __init__(self, data):
+        self.childern = []
         self.data = data
 
 class treenodeFunction:
@@ -20,7 +22,7 @@ class treenodeFunction:
         
         pendingNode = Queue() #  3 
         pendingNode.put(root)
-
+        
         while(pendingNode.qsize() > 0):
             front = pendingNode.get() #   
             n = int(input(f"how many Child do u want to enter : {front.data} : "))
@@ -28,11 +30,43 @@ class treenodeFunction:
                 for i in range(0,n):
                     data = int(input(f'Enter the {front.data} childrens data : '))
                     newNode = treenode(data)
-                    front.children.append(newNode)
+                    front.childern.append(newNode)
                     pendingNode.put(newNode)
         return root
 
+    # def DisplayLevel(self, root):
+    #     if(root == None):
+    #         return
+        
+    #     print("Root Data : ",root.data)
+    #     pendingNode = Queue() 
+    #     pendingNode.put(root)
+
+    #     while(pendingNode.qsize() > 0):
+    #         front = pendingNode.get()
+    #         print(len(front.children))
+    #         for i in range(0, len(front.children)):
+    #             print("Children of :",front.data," ",front.children[i].data, end=" ")
+    #             pendingNode.put(front.children[i])
+    #         print()
+
+    # def display(self, root):
+    #     if root is None:
+    #         return
+    #     print("Root data:", root.data)
+    #     pendingQueue = Queue()
+    #     pendingQueue.put(root)
+
+    #     while pendingQueue.qsize() > 0:
+    #         front = pendingQueue.get()
+    #         for i in range(len(front.childern)):
+    #             print("childern of:", front.data, ":", front.childern[i].data, end=" | ")
+    #             pendingQueue.put(front.childern[i])
+    #         print()
+
 ob = treenodeFunction()
 root = ob.creatTree()
+ob.display(root)
+
 
 
