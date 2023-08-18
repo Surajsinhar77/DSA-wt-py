9723d87c94631e970d91f82ee7336fdcac67b7a2
Suraj Kumar <surajsinhaking77@gmail.com>
2023-08-18 21:26:01 +0530

    priority queue min remove

--- PriorityQueue/PQueue.py
+++ PriorityQueue/PQueue.py
@@ -1,12 +1,12 @@
 
 class PriorityQueue:
-    pq = []
-
     def __init__(self):
         self.pq =[]
 
     def isEmpty(self):
-        return self.pq
+        if(len(self.pq) == 0):
+            return True
+        return False
     
     def PqSize(self):
         return len(self.pq)
@@ -16,13 +16,13 @@ class PriorityQueue:
             return 0
         return self.pq[0]
     
-    def insert(self , element ):
+    def insertInPq(self , element ):
         self.pq.append(element)
 
         childInd = len(self.pq)-1
 
         while(childInd > 0):
-            parentIndex = childInd-1//2
+            parentIndex = (childInd-1)//2
             if(self.pq[childInd] < self.pq[parentIndex]):
                 temp = self.pq[childInd]
                 self.pq[childInd] = self.pq[parentIndex]
@@ -35,6 +35,9 @@ class PriorityQueue:
         if(len(self.pq) < 1):
             return 0
         
+        if(len(self.pq) == 1):
+            return self.pq.pop()
+
         minValue = self.pq[0]
         self.pq[0] = self.pq[len(self.pq)-1]
         self.pq.pop()
@@ -43,8 +46,9 @@ class PriorityQueue:
         left = (currentIndex*2)+1 
         right = (currentIndex*2)+2
 
-        while(currentIndex < left):
-            if(self.pq[left] < self.pq[right]):
+        while(currentIndex < len(self.pq)):
+            print(left, "            ", right ,end=" ")
+            if(left < len(self.pq) and right < len(self.pq)  and  self.pq[left] < self.pq[right] ):
                 if(self.pq[left] < self.pq[currentIndex]):
                     temp = self.pq[left]
                     self.pq[left] = self.pq[currentIndex]
@@ -52,7 +56,7 @@ class PriorityQueue:
                     currentIndex = left
                 else:
                     break
-            else:
+            elif(right < len(self.pq)):
                 if(self.pq[right] < self.pq[currentIndex]):
                     temp = self.pq[right]
                     self.pq[right] = self.pq[currentIndex]
@@ -60,6 +64,34 @@ class PriorityQueue:
                     currentIndex = right
                 else:
                     break
+            else:
+                break
             left = (currentIndex*2)+1
             right = (currentIndex*2)+2
         return minValue
+    
+    def displayPriorityQueue(self):
+        print(self.pq)
+
+ob  = PriorityQueue()
+ob.insertInPq(2)
+ob.insertInPq(1)
+ob.insertInPq(4)
+ob.insertInPq(3)
+print(ob.getMin())
+ob.displayPriorityQueue()
+
+ob.insertInPq(9)
+ob.insertInPq(11)
+ob.insertInPq(7)
+print("size of the PQ : ",ob.PqSize())
+
+print(ob.PqSize())
+print(ob.removeMin())
+print(ob.removeMin())
+print(ob.removeMin())
+print(ob.removeMin())
+print(ob.removeMin())
+print(ob.removeMin())
+print(ob.removeMin())
+ob.displayPriorityQueue()
