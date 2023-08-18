2e98a0ff2a7d3f916bbd63ebd379fe02d307b704
Suraj Kumar <surajsinhaking77@gmail.com>
2023-08-18 20:47:02 +0530

    done this

--- PriorityQueue/PQueue.py
+++ PriorityQueue/PQueue.py
@@ -30,3 +30,35 @@ class PriorityQueue:
             else:
                 break
             childInd = parentIndex
+
+    def removeMin(self):
+        if(len(self.pq) < 1):
+            return 0
+        
+        minValue = self.pq[0]
+        self.pq[0] = self.pq[len(self.pq)-1]
+        self.pq.pop()
+
+        currentIndex = 0
+        left = (currentIndex*2)+1 
+        right = (currentIndex*2)+2
+
+        while(currentIndex < left):
+            if(self.pq[left] < self.pq[right]):
+                if(self.pq[left] < self.pq[currentIndex]):
+                    temp = self.pq[left]
+                    self.pq[left] = self.pq[currentIndex]
+                    self.pq[currentIndex] = temp
+                    currentIndex = left
+                else:
+                    break
+            else:
+                if(self.pq[right] < self.pq[currentIndex]):
+                    temp = self.pq[right]
+                    self.pq[right] = self.pq[currentIndex]
+                    self.pq[currentIndex] = temp
+                    currentIndex = right
+                else:
+                    break
+            left = (currentIndex*2)+1
+            right = (currentIndex*2)+2
